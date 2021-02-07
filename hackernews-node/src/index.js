eab6ce50b8cb9e093f3b487b3eeb2d546e3ae72b
Alison Reilly <reillyamr@gmail.com>
2021-02-07 12:27:41 -0500

    add exp, show graphql chking schema agains resolve

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -12,6 +12,16 @@ const resolvers = {
     }
 }
 
+// uncomment below and comment out above to see
+// how graphql will adhere to the schema created in typedefs
+// typedefs defined that info will be some sort of string
+// by doing below, graphql will return an error
+// const resolvers = {
+//     Query: {
+//       info: () => null,
+//     }
+//   }
+
 const server = new ApolloServer ({
     typeDefs,
     resolvers,
