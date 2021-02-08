c45b8c5fb7baa997909b1b9d49cd5f1ee3b105d0
Alison Reilly <reillyamr@gmail.com>
2021-02-07 19:27:51 -0500

    add a new link to db to view data

--- hackernews-node/src/script.js
+++ hackernews-node/src/script.js
@@ -4,6 +4,12 @@ const prisma = new PrismaClient()
 
 //async func to send queiries to db, all queries willl go here
 async function main() {
+    const newLink = await prisma.link.create({
+        data: {
+          description: 'Fullstack tutorial for GraphQL',
+          url: 'www.howtographql.com',
+        },
+      })
     const allLinks = await prisma.link.findMany()
     console.log(allLinks)
 }
